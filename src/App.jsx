b5126aa482113051de4be2c45ac11bc81974c9b5
milan0827 <milanrokaya2014@gmail.com>
2023-11-03 02:52:07 +0545

    more components added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Work from "./Components/Work/Work";
 
 import "./App.css";
 import GapLine from "./Components/GapLine/GapLine";
+import Service from "./Components/Service/Service";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -15,6 +17,10 @@ function App() {
       <About />
       <GapLine />
       <Work />
+      <GapLine />
+      <Service />
+      <GapLine />
+      <Footer />
     </div>
   );
 }
