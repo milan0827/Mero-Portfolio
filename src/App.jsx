2d33fa0f6797712528786879c77552c41019c278
milan0827 <milanrokaya2014@gmail.com>
2023-11-03 01:14:59 +0545

    Gapline added

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import About from "./Components/About/About";
 import Work from "./Components/Work/Work";
 
 import "./App.css";
+import GapLine from "./Components/GapLine/GapLine";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <HeroSection />
-
+      <GapLine />
       <About />
+      <GapLine />
       <Work />
     </div>
   );
